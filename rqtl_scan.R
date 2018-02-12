e2f0d327dd7e5cf9ee4f62a0321e7847099d096e
kathiesun <sunk@med.unc.edu>
2018-02-12 16:24:51 -0500

    sdf

--- rqtl_scan.R
+++ rqtl_scan.R
@@ -2,33 +2,59 @@ setwd("/nas/depts/006/valdar-lab/users/sunk/rqtl_do/")
 library(qtl2)
 library(tidyverse)
 
-
-######### scans ######### 
-pr1 <- readRDS("rqtl2_do1_probs.rds")
-pr2 <- readRDS("rqtl2_do2_probs.rds")
+## read in data
+pr <- readRDS("rqtl2_do12_probs.rds")
+#pr1 <- readRDS("rqtl2_do1_probs.rds")
+#pr2 <- readRDS("rqtl2_do2_probs.rds")
 pheno <- readRDS("DO_pheno_only.rds")
 covar <- readRDS("DO_covar.rds")
+
+## create pseudomarker mapping object
+gmap <- read.csv("GM/GM/GM_info.csv", stringsAsFactors = T)
+gmap$marker <- as.character(gmap$marker)
+gmap %>% select(marker, chr, cM) -> gmap
+gmap_list <- list()
+for(i in 1:length(unique(gmap$chr))){
+  gmap_list[[i]] <- gmap[which(gmap$chr == 1), 3]
+  names(gmap_list[[i]]) <- gmap[which(gmap$chr == 1), "marker"]
+}
+names(gmap_list) <- unique(gmap$chr)
+map <- insert_pseudomarkers(gmap_list, step=1)
+
+
+
+######### scans ######### 
+rownames(covar) <- covar$MouseID
+rownames(pheno) <- pheno$MouseID
 covar$Sex <- ifelse(covar$Sex == "Female", 1, 0)
-covar <- data.matrix(covar)
-rownames(covar) <- covar[,1]
-covar1 <- covar[which(covar[,"DO"] == 1),]
-covar2 <- covar[which(covar[,"DO"] == 2),]
 
-pheno %>% filter(MouseID %in% rownames(covar1)) -> pheno1
-pheno1 <- select(pheno1, MouseID, starts_with('X'))
-pheno1 <- data.matrix(pheno1)
-rownames(pheno1) <- pheno1[,1]
+covar %>% 
+  filter(DO == 1) %>% 
+  select(MouseID, Sex) -> covar1
 
+pheno %>% filter(MouseID %in% covar1$MouseID) %>%
+  select(MouseID, ends_with('56')) -> pheno1
 
+rownames(covar1) <- covar1$MouseID
+rownames(pheno1) <- pheno1$MouseID
+covar1 <- data.matrix(covar1[,-1])
+pheno1 <- data.matrix(pheno1[,-1])
 
-covar %>% filter(id %in% covar) -> covar1
-covar %>% filter(id %in% rownames(pr2[[1]])) -> covar2
-pheno %>% filter(id %in% rownames(pr1[[1]])) -> pheno1
-pheno %>% filter(id %in% rownames(pr2[[1]])) -> pheno2
+out <- scan1(pr, pheno1)
 
-pheno1 <- data.frame(pheno1[match(rownames(pr1[[1]]), pheno1$id), ])
-pheno1 <- rbind(colnames(pheno1), pheno1)
-out <- scan12(pr1, pheno1, addcovar=covar1, cores=8)
+## plot
+color <- c("slateblue", "violetred", "seagreen3", "steelblue1")
+par(mar=c(5.1, 4.1, 1.1, 1.1))
+ymx <- maxlod(out) # overall maximum LOD score
+
+for(i in 1:ncol(out)){
+  add=T
+  if(i==1) add=F
+  plot(out, map, lodcolumn=1, col=color[i], ylim=c(0, ymx*1.02), add=add)
+}
+legend("topleft", lwd=2, col=paste(color), colnames(out), bg="gray90")
+
+find_peaks(out, map, threshold=4, drop=1.5)
 
 ######### calculate kinship mat ######### 
 pr1 <- lapply(pr, function(x) x[which(rownames(x) %in% pheno1$MouseID), , ])
